88d39979bc117947df53187fbcb5a5e4e465d724
James Ravn <james@r-vn.org>
2022-02-22 20:29:47 +0000

    Add missing packages needed by moderncv

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@ let
   pkgs = import <nixpkgs-unstable> {};
 in pkgs.mkShell {
   buildInputs = with pkgs; [
-    (texlive.combine { inherit (texlive) scheme-medium moderncv; })
+    (texlive.combine { inherit (texlive) scheme-medium moderncv fontawesome5 multirow arydshln; })
   ];
 }
